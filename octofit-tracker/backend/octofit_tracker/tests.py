3c802a56c1fa06385625ab91606ab7115ffabf07
bijalchetanmaniar <bijal.chetan.maniar@accenture.com>
2026-02-17 11:09:07 +0000

    feat(octofit): scaffold models, serializers, views, admin, tests, and urls for all collections

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class UserModelTest(TestCase):
+    def test_create_user(self):
+        team = Team.objects.create(name='Marvel')
+        user = User.objects.create(email='hero@marvel.com', username='IronMan', team=team)
+        self.assertEqual(user.email, 'hero@marvel.com')
+
+class TeamModelTest(TestCase):
+    def test_create_team(self):
+        team = Team.objects.create(name='DC')
+        self.assertEqual(team.name, 'DC')
+
+class ActivityModelTest(TestCase):
+    def test_create_activity(self):
+        team = Team.objects.create(name='Marvel')
+        user = User.objects.create(email='hero@marvel.com', username='IronMan', team=team)
+        activity = Activity.objects.create(user=user, type='Running', duration=30, date='2026-02-17')
+        self.assertEqual(activity.type, 'Running')
+
+class WorkoutModelTest(TestCase):
+    def test_create_workout(self):
+        workout = Workout.objects.create(name='Pushups', description='Do 20 pushups')
+        self.assertEqual(workout.name, 'Pushups')
+
+class LeaderboardModelTest(TestCase):
+    def test_create_leaderboard(self):
+        team = Team.objects.create(name='Marvel')
+        leaderboard = Leaderboard.objects.create(team=team, points=100)
+        self.assertEqual(leaderboard.points, 100)
